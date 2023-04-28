308a03cc8b81cac0ef67e316064734c6ee099670
AaronW <aaronwang96@hotmail.com>
2023-04-28 15:16:58 -0700

    serving react frontend through flask

--- server/app.py
+++ server/app.py
@@ -1,10 +1,10 @@
 import os
 import time
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from models import db
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../client/build')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -12,9 +12,13 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 @app.route('/time')
 def get_current_time():
