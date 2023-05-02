933567146439b883121f7c988d5f55459dd54fab
AaronW <aaronwang96@hotmail.com>
2023-05-01 22:47:54 -0700

    changed stack to grid to accomodate more products

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Grid';
 import { Box, Paper, Button } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 
@@ -8,9 +8,10 @@ function Products(props) {
 
   return (
     <Box align='center' sx={{ marginTop: 3, maxWidth: '900px' }}>
-      <Stack direction='row' spacing={2} sx={{ justifyContent: 'center' }}>
-          {props.currentProducts && props.currentProducts.map((item) => (
-            <Paper key={item.id} elevation={3} sx={{ paddingX: 1, paddingBottom: 1 }}>
+      <Grid container spacing={2} justifyContent='center' sx={{ marginTop: 3, maxWidth: '900px' }}>
+        {props.currentProducts && props.currentProducts.map((item) => (
+          <Grid item xs={12} sm={6} md={4} key={item.id}>
+            <Paper key={item.id} elevation={3} sx={{ padding: 1 }}>
                 <h2>{item.name}</h2>
                 <p>{item.description}</p>
                 <p>{item.price}</p>
@@ -23,12 +24,10 @@ function Products(props) {
                     Purchase
                   </Button>
                 )}
-                {/* <Button style={{ color: theme.palette.secondary.main }}>
-                    Purchase
-                </Button> */}
             </Paper>
-          ))}
-        </Stack>
+          </Grid>
+        ))}
+      </Grid>
     </Box>
   );
 }
